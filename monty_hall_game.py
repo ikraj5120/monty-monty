b02d207ac057bf29a054d324ada17156e5c3fd0e
ikraj5120 <39771313+ikraj5120@users.noreply.github.com>
2019-06-24 10:24:06 +0530

    Add files via upload

---
+++ monty_hall_game.py
@@ -0,0 +1,30 @@
+import random
+doors = [1,2,3]
+prize = random.choice(doors)
+print(prize)
+user_choice = int(input("Enter your choice (1,2,3): "))
+if user_choice==prize:
+    doors.remove(user_choice)
+    door_open = random.choice(doors)
+    print(f"\nBehind door {door_open} is a Goat....")
+    switch = input("\nDo you want to switch (y/n): ")
+    if switch == 'y':
+        doors.remove(door_open)
+        remaining = doors[0]
+        print(f"\nBehind door {remaining} is a Goat....")
+        print("\nOops! You have lost. Try again....")
+    else:
+        print("\nBehind door {user_choice} is a Car....")
+        print("\nCongratulations! You have won...")
+else:
+    doors.remove(user_choice)
+    doors.remove(prize)
+    door_open = doors[0]
+    print(f"\nBehind door {door_open} is a Goat....")
+    switch = input("\nDo you want to switch (y/n): ")
+    if switch == 'y':
+        print(f"\nBehind door {prize} is a Car..")
+        print("\nCongratulations! You have won...")
+    else:
+        print(f"Behind door {user_choice} is a Goat....")
+        print("Oops! You have lost. Try again....")
---
+++ monty_hall_simulation.py
@@ -0,0 +1,51 @@
+import random
+
+def not_switch():
+    doors = [1,2,3]
+    choice = random.choice(doors)
+    prize = random.choice(doors)
+    
+    if choice == prize:
+        return 1
+    else:
+        return 0
+
+def switch():
+    doors = [1,2,3]
+    choice = random.choice(doors)
+    prize = random.choice(doors)
+
+    if choice != prize:
+        eleminate_doors = [1,2,3]
+        eleminate_doors.remove(prize)
+        eleminate_doors.remove(choice)
+        doors.remove(eleminate_doors[0])
+        doors.remove(choice)
+        second_choice = doors[0]
+        if second_choice == prize:
+            return 1
+        else:
+            return 0
+    else:
+        eleminate_doors = [1,2,3]
+        eleminate_doors.remove(choice)
+        doors.remove(random.choice(eleminate_doors))
+        doors.remove(choice)
+        second_choice = doors[0]
+        if second_choice == prize:
+            return 1
+        else:
+            return 0
+
+def simulation():
+    trials = 100000
+    switch_result = 0
+    non_switch_result = 0
+    for i in range(trials):
+        switch_result += switch()
+        non_switch_result += not_switch()
+    print("Non Switched wins: ",non_switch_result)
+    print("Switched wins: ",switch_result)
+
+if __name__=='__main__':
+    simulation()
